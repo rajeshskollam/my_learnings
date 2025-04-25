499d8d35dd9848816982d44611223420c96332b9
Rajesh Soman Pillai <rajeshskollam@hotmail.com>
2025-04-25 07:50:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,184 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+// Connerstone Import
+import * as cornerstone from "cornerstone-core";
+import * as cornerstoneMath from "cornerstone-math";
+import * as cornerstoneTools from "cornerstone-tools";
+import Hammer from "hammerjs";
+import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
+
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+cornerstoneTools.external.Hammer = Hammer;
+
+const imageId =
+  "https://rawgit.com/cornerstonejs/cornerstoneWebImageLoader/master/examples/Renal_Cell_Carcinoma.jpg";
+
+const divStyle = {
+  width: "512px",
+  height: "512px",
+  position: "relative",
+  color: "white"
+};
+
+const bottomLeftStyle = {
+  bottom: "5px",
+  left: "5px",
+  position: "absolute",
+  color: "white"
+};
+
+const bottomRightStyle = {
+  bottom: "5px",
+  right: "5px",
+  position: "absolute",
+  color: "white"
+};
+
+const stack = {
+  imageIds: [imageId],
+  currentImageIdIndex: 0
+};
+
+class CornerstoneElement extends React.Component {
+  constructor(props){
+    console.log("CornerstoneElement Constructor Started...");
+    console.log(props);
+    super(props);
+
+    this.state = {
+      stack: props.stack,
+      viewport: cornerstone.getDefaultViewport(null, undefined),
+      imageId: props.stack.imageIds[0]
+    };
+
+    this.onImageRendered = this.onImageRendered.bind(this);
+    this.onNewImage = this.onNewImage.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="viewportElement" 
+          style={divStyle}
+          ref={input => {
+            this.element = input;
+            }}>
+              <canvas className="cornerstone-canvas" />
+              <div style={bottomLeftStyle}>Zoom: {this.state.viewport.scale}</div>
+              <div style={bottomRightStyle}>
+                WW/WC: {this.state.viewport.voi.windowWidth} /{" "}
+                {this.state.viewport.voi.windowCenter}
+              </div>
+        </div>
+      </div>
+    );
+  }
+
+  onWindowResize() {
+    console.log("onWindowResize");
+    cornerstone.resize(this.element);
+  }
+
+  onImageRendered() {
+    const viewport = cornerstone.getViewport(this.element);
+    console.log(viewport);
+
+    this.setState({
+      viewport
+    });
+
+    console.log(this.state.viewport);
+  }
+
+  onNewImage() {
+    const enabledElement = cornerstone.getEnabledElement(this.element);
+
+    this.setState({
+      imageId: enabledElement.image.imageId
+    });
+  }
+
+  onMouseClick(event){
+    const { detail } = event;
+    const { image, currentPoints } = detail;
+    const imageCoords = currentPoints.image;
+
+    console.log('Clicked at image coordinates:', imageCoords);
+    console.log('Clicked at image:', image);
+  }
+
+  componentDidMount() {
+    const element = this.element;
+
+    // Enable the DOM Element for use with Cornerstone
+    cornerstone.enable(element);
+
+    // Load the first image in the stack
+    cornerstone.loadImage(this.state.imageId).then(image => {
+      // Display the first image
+      cornerstone.displayImage(element, image);
+
+      // Add the stack tool state to the enabled element
+      const stack = this.props.stack;
+      cornerstoneTools.addStackStateManager(element, ["stack"]);
+      cornerstoneTools.addToolState(element, "stack", stack);
+
+      cornerstoneTools.mouseInput.enable(element);
+      cornerstoneTools.mouseWheelInput.enable(element);
+      cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+      cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+      cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+      cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+
+      cornerstoneTools.touchInput.enable(element);
+      cornerstoneTools.panTouchDrag.activate(element);
+      cornerstoneTools.zoomTouchPinch.activate(element);
+
+      element.addEventListener(
+        "cornerstoneimagerendered",
+        this.onImageRendered
+      );
+      element.addEventListener("cornerstonenewimage", this.onNewImage);
+      element.addEventListener("cornerstonetoolsmouseclick", this.onMouseClick);
+      window.addEventListener("resize", this.onWindowResize);
+    });
+  }
+
+  componentWillUnmount() {
+    const element = this.element;
+    element.removeEventListener(
+      "cornerstoneimagerendered",
+      this.onImageRendered
+    );
+
+    element.removeEventListener("cornerstonenewimage", this.onNewImage);
+    element.removeEventListener("cornerstonetoolsmouseclick", this.onMouseClick);
+    window.removeEventListener("resize", this.onWindowResize);
+
+    cornerstone.disable(element);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const stackData = cornerstoneTools.getToolState(this.element, "stack");
+    const stack = stackData.data[0];
+    stack.currentImageIdIndex = this.state.stack.currentImageIdIndex;
+    stack.imageIds = this.state.stack.imageIds;
+    cornerstoneTools.addToolState(this.element, "stack", stack);
+
+    //const imageId = stack.imageIds[stack.currentImageIdIndex];
+    //cornerstoneTools.scrollToIndex(this.element, stack.currentImageIdIndex);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Cornerstone React Component Example</h2>
+      <CornerstoneElement stack={{...stack}}/>
     </div>
   );
 }
